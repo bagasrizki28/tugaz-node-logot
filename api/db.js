173259622775ae5060c755e12b652d343b30cec1
bagasrizki28 <bagasrizki700@gmail.com>
2023-03-23 15:58:29 +0700

    done

---
+++ api/db.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import pkg from "pg";
+
+const { Client } = pkg;
+
+export const client = new Client({
+  // port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+await client.connect();
+console.log("Terhubung ke basis data.");
